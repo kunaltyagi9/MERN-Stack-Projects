aecf73f9aa91d017c7b96d6b9849c407a1f79baf
Kunal Tyagi <kunaltyagi93@gmail.com>
2022-04-23 23:10:22 +0530

    removed console logs

--- Inshorts-Clone/server/connection/db.js
+++ Inshorts-Clone/server/connection/db.js
@@ -1,10 +1,8 @@
 import mongoose from 'mongoose';
 
 const Connection = async (username, password) => {
-    console.log(username, password);
     try {   
         const URL = `mongodb://${username}:${password}@clone-inshort-shard-00-00.deivq.mongodb.net:27017,clone-inshort-shard-00-01.deivq.mongodb.net:27017,clone-inshort-shard-00-02.deivq.mongodb.net:27017/INSHORTS-CLONE?ssl=true&replicaSet=atlas-ytsxi5-shard-0&authSource=admin&retryWrites=true&w=majority`
-        
         await mongoose.connect(URL, { useNewUrlParser: true })
         
         console.log('Database connected successfully');
