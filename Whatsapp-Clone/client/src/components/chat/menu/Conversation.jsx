6f0f5705eaca92403c8f6fa1ca82dfad073d2096
Kunal Tyagi <kunaltyagi93@gmail.com>
2022-08-21 19:52:25 +0530

    removed logs

--- Whatsapp-Clone/client/src/components/chat/menu/Conversation.jsx
+++ Whatsapp-Clone/client/src/components/chat/menu/Conversation.jsx
@@ -52,7 +52,7 @@ const Conversation = ({ user }) => {
     useEffect(() => {
         const getConversationMessage = async() => {
             const data = await getConversation({ senderId: account.sub, receiverId: user.sub });
-            setMessage({ text: data.message, timestamp: data.updatedAt });
+            setMessage({ text: data?.message, timestamp: data?.updatedAt });
         }
         getConversationMessage();
     }, [newMessageFlag]);
