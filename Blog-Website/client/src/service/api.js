73626c2131f236603d1ffca3fbb2b1f1d65f4bf3
Ansari Irfan <115148633+Ansari-Irfan-360@users.noreply.github.com>
2024-04-01 16:40:41 +0530

    Bug fixed
    It passes an object not a string

--- Blog-Website/client/src/service/api.js
+++ Blog-Website/client/src/service/api.js
@@ -118,7 +118,7 @@ for (const [key, value] of Object.entries(SERVICE_URLS)) {
         axiosInstance({
             method: value.method,
             url: value.url,
-            data: value.method === 'DELETE' ? '' : body,
+            data: value.method === 'DELETE' ? {} : body,
             responseType: value.responseType,
             headers: {
                 authorization: getAccessToken(),
